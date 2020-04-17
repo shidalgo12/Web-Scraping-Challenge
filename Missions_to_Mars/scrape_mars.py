1862568ae6de33cd333acce0dee85b6ea28ea5ee
unknown <shidalgo1212@gmail.com>
2020-04-17 16:37:50 -0500

    .html page layout, updated news, featured image, and mars hemispheres

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -29,16 +29,6 @@ def scrape_info():
   mars["news"]=news[1].text
   mars["paragraph"]=paragraph
 
-  # # Store data in a dictionary
-  # mars = {
-  #   "news": news[1].text,
-  #   "paragraph": paragraph,
-  #   # "table": fact_table
-  # }
-
-  # # Close the browser after scraping
-  # browser.quit()
-
   # ***MARS FEATURED IMAGE***
   # =========================
   # Visit news website
@@ -61,12 +51,11 @@ def scrape_info():
 
   # Add featured_image_url to master dictionary
   mars['featured_image_url']= featured_image_url
-  print(mars)
-  # Close the browser after scraping
-  # browser.quit()
+  # print(mars)
+  
 
-  # ***MARS WEATHER***
-  # ==================
+  # # ***MARS WEATHER***
+  # # ==================
   # # Visit Twitter website
   # weather_url = 'https://twitter.com/marswxreport?lang=en'
   # browser.visit(weather_url)
@@ -81,9 +70,6 @@ def scrape_info():
   # # Add variable to master dictionary
   # mars["mars_weather"]=mars_weather
   
-  # # Close the browser after scraping
-  # browser.quit()
-
   # ***MARS FACT TABLE***
   # =====================
   # # Visit Fact Table Website
@@ -97,13 +83,123 @@ def scrape_info():
   # # Retrieve Mars Fact Table
   # fact_table = soup.find('table', class_='tablepress tablepress-id-p-mars')
 
-  # # Close the browser after scraping
-  # browser.quit()
+  # # Add variable to master dictionary
+  # mars['fact_table']= fact_table
+  # print(mars)
 
   # ***MARS HEMISPHERE IMAGES***
   # ============================
 
-  # Return results
+  # Links to Hemisphere websites
+  hem1_url = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced'
+  hem2_url = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/schiaparelli_enhanced'
+  hem3_url = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/syrtis_major_enhanced'
+  hem4_url = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/valles_marineris_enhanced'
+  # ***HEMISPHERE IMAGE 1***
+  # Retrieve all elements that contain hemisphere image and title
+  browser.visit(hem1_url)
+
+  # Scrape page into Soup
+  html = browser.html
+  soup = bs(html, "html.parser")
+
+  # Retrieve all elements that contain hemisphere image and title
+  content = soup.find('section', class_='block metadata')
+  # print(content)
+
+  title1 = content.find('h2').text
+  # print(title1)
+
+  img_content = soup.find('div', class_='downloads')
+  img_ul = img_content.find("ul")
+  img_li= img_ul.find("li")
+  img_link = img_li.find("a")
+  href1 = img_link["href"]
+
+  # hem_img_url
+  mars['href1']= href1
+  mars['title1']= title1
+  
+  # ***HEMISPHEERE IMAGE 2***
+  # Retrieve all elements that contain hemisphere image and title
+  browser.visit(hem2_url)
+
+  # Scrape page into Soup
+  html = browser.html
+  soup = bs(html, "html.parser")
+
+  # Retrieve all elements that contain hemisphere image and title
+  content = soup.find('section', class_='block metadata')
+  # print(content)
+
+  title2 = content.find('h2').text
+  # print(title1)
+
+  img_content = soup.find('div', class_='downloads')
+  img_ul = img_content.find("ul")
+  img_li= img_ul.find("li")
+  img_link = img_li.find("a")
+  href2 = img_link["href"]
+  # print(img_href1)
+
+  # hem_img_url
+  mars['href2']= href2
+  mars['title2']= title2
+
+  # ***HEMISPHEERE IMAGE 3***
+  # Retrieve all elements that contain hemisphere image and title
+  browser.visit(hem3_url)
+
+  # Scrape page into Soup
+  html = browser.html
+  soup = bs(html, "html.parser")
+
+  # Retrieve all elements that contain hemisphere image and title
+  content = soup.find('section', class_='block metadata')
+  # print(content)
+
+  title3 = content.find('h2').text
+  # print(title1)
+
+  img_content = soup.find('div', class_='downloads')
+  img_ul = img_content.find("ul")
+  img_li= img_ul.find("li")
+  img_link = img_li.find("a")
+  href3 = img_link["href"]
+
+  # hem_img_url
+  mars['href3']= href3
+  mars['title3']= title3
+
+  # ***HEMISPHEERE IMAGE 4***
+  # Retrieve all elements that contain hemisphere image and title
+  browser.visit(hem4_url)
+
+  # Scrape page into Soup
+  html = browser.html
+  soup = bs(html, "html.parser")
+
+  # Retrieve all elements that contain hemisphere image and title
+  content = soup.find('section', class_='block metadata')
+  # print(content)
+
+  title4 = content.find('h2').text
+  # print(title1)
+
+  img_content = soup.find('div', class_='downloads')
+  img_ul = img_content.find("ul")
+  img_li= img_ul.find("li")
+  img_link = img_li.find("a")
+  href4 = img_link["href"]
+
+  # hem_img_url
+  mars['href4']= href4
+  mars['title4']= title4
+  
+  # Close browser after all web scrappings
+  browser.quit()
+
+  # Add Web Scrappings to MongoDB
   return mars
 
 if __name__ == "__main__":
